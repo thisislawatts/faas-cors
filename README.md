e13649106e8ecc1bc20735d53af1c9516fb17493
Luke Watts <luke@snyk.io>
2018-12-09 11:30:37 +0100

    chore: adds linting

---
+++ README.md
@@ -0,0 +1,10 @@
+Faas CORS
+===
+
+[![Known Vulnerabilities](https://snyk.io/test/github/thisislawatts/faas-cors/badge.svg?targetFile=package.json)](https://snyk.io/test/github/thisislawatts/faas-cors?targetFile=package.json)
+
+
+Deployment
+---
+
+```gcloud functions deploy badge --runtime nodejs8 --trigger-http```
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const r2 = require('r2');
 
 module.exports.badge = async (req, res) => {
-  const url = req.body.url || 'https://snyk.io/test/npm/snyk/latest/badge.svg';
-  const str = await r2(url).text
+  const url = req.body.url;
+
+  if (!url) {
+    return res.status(400).send('');
+  }
+
+  const str = await r2(url).text;
 
   res.set('Access-Control-Allow-Origin', '*');
 
   return res.send(str);
-}
+};
